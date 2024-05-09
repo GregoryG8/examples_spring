1d5de065a112077899a2c10c854e592c4a5ced11
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-09 14:03:48 -0500

    Initial commit

---
+++ src/main/java/examples_spring/examples_spring/ExamplesSpringApplication.java
@@ -0,0 +1,13 @@
+package examples_spring.examples_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/examples_spring/examples_spring/ExamplesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package examples_spring.examples_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
